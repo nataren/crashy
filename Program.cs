0570e6e0fb26f5a067a24ce852dc57c4576de16b
César López-Natarén <cesarn@mindtouch.com>
2015-03-05 17:56:43 -0800

    First testc case

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using MindTouch.Collections;
+
+namespace Crashy {
+    class Program {
+        static void Project(string item) { }
+        static void Main() {
+            var queue = new ProcessingQueue<string>(Project, 1);     
+            while(true) {
+                queue.TryEnqueue(new Guid().ToString());
+            }
+        }
+    }
+}
